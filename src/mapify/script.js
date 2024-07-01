2c1895b2571e88610d39d6fced86078ca140eeb9
TANMAYAGARWALLSKT <131980930+TANMAYAGARWALLSKT@users.noreply.github.com>
2024-07-02 00:15:09 +0530

    basic map

--- src/mapify/script.js
+++ src/mapify/script.js
@@ -11,37 +11,6 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-let map;
-
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(
-    function (position) {
-      const { latitude } = position.coords;
-      const { longitude } = position.coords;
-      console.log(`https://www.google.pt/maps/@${latitude},${longitude}`);
-
-      const coords = [latitude, longitude];
-
-      map = L.map('map').setView(coords, 16);
-
-      L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-        attribution:
-          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      }).addTo(map);
-
-      L.marker(coords).addTo(map).bindPopup('I am here.').openPopup();
-//handling click on map
-      map.on('click', function (mapEvent) {
-        form.classList.remove('hidden');
-        inputDistance.focus();
-      });
-    },
-    function () {
-      alert('Could not get your position');
-    }
-  );
-}
-
 form.addEventListener('submit', function () {
   // display marker
   const { lat, lng } = mapEvent.latlng;
@@ -61,10 +30,11 @@ form.addEventListener('submit', function () {
     .openPopup();
 });
 
-
 const sidebar = document.getElementById('sidebar');
 const sidebarToggle = document.getElementById('sidebarToggle');
 
 sidebarToggle.addEventListener('click', () => {
   sidebar.classList.toggle('-translate-x-full');
 });
+
+ 
